c0c1ed7f7950b8ff72778d196a12c648066bd437
Hemaxi Solanki <104411100+H198a@users.noreply.github.com>
2025-06-13 02:17:05 +0530

    Update blur.swift

--- blur.swift
+++ blur.swift
@@ -79,3 +79,166 @@ class ViewController: UIViewController {
     }
     
 }
+
+
+
+import UIKit
+
+class ViewController: UIViewController, UITabBarControllerDelegate {
+    var popupVC: PopupVC? = nil
+    var blurView: UIVisualEffectView? = nil
+    var tabView1: tab1!
+    var tabView2: tab2!
+    var isSelectionTabVisible = false
+    var customTabBarContainer: UIView!
+    var customTabBarVC: CustomTabBarController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.tabBarController?.tabBar.isHidden = false  // Hide system tab bar
+        setUP()
+//        setupCustomTabBars()
+//               showTab1()
+    }
+    func setUP(){
+        customTabBarVC = CustomTabBarController()
+               addChild(customTabBarVC)
+               view.addSubview(customTabBarVC.view)
+               customTabBarVC.didMove(toParent: self)
+
+               customTabBarVC.delegate = self
+
+               NSLayoutConstraint.activate([
+                   customTabBarVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                   customTabBarVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                   customTabBarVC.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                   customTabBarVC.view.heightAnchor.constraint(equalToConstant: 80)
+               ])
+    }
+    @IBAction func onClickSelectAll(_ sender: Any) {
+        isSelectionTabVisible.toggle()
+//                if isSelectionTabVisible {
+//                    showTab2()
+//                } else {
+//                    showTab1()
+//                }
+        customTabBarVC.toggleTabs()
+    }
+    
+//    func setupCustomTabBars() {
+//           let height: CGFloat = 80
+//           let yPosition = self.view.frame.height - height
+//           customTabBarContainer = UIView(frame: CGRect(x: 0, y: yPosition, width: view.frame.width, height: height))
+//           customTabBarContainer.backgroundColor = .clear
+//           self.view.addSubview(customTabBarContainer)
+//        
+//
+//           if let view1 = Bundle.main.loadNibNamed("tab1", owner: self, options: nil)?.first as? tab1 {
+//               tabView1 = view1
+//               tabView1.frame = customTabBarContainer.bounds
+//               tabView1.tap = self
+//           }
+//
+//           if let view2 = Bundle.main.loadNibNamed("tab2", owner: self, options: nil)?.first as? tab2 {
+//               tabView2 = view2
+//               tabView2.frame = customTabBarContainer.bounds
+//           }
+//       }
+//    func showTab1() {
+//            clearTabBar()
+//            customTabBarContainer.addSubview(tabView1)
+//        }
+//
+//        func showTab2() {
+//            clearTabBar()
+//            customTabBarContainer.addSubview(tabView2)
+//        }
+//
+//        func clearTabBar() {
+//            for subview in customTabBarContainer.subviews {
+//                subview.removeFromSuperview()
+//            }
+//        }
+
+    @IBAction func onClickClose(_ sender: Any) {
+        self.removeFromParent()
+        self.popupVC?.view.removeFromSuperview()
+        popupVC = nil
+    }
+    
+}
+
+extension ViewController: delegateFunc{
+    func didTap() {
+        if popupVC == nil {
+            print("Opening popup")
+            guard let popup = storyboard?.instantiateViewController(withIdentifier: "PopupVC") as? PopupVC else { return }
+            self.popupVC = popup
+            popup.modalPresentationStyle = .overCurrentContext
+            self.present(popup, animated: true, completion: nil)
+            
+        } else {
+            print("Closing popup")
+            self.removeFromParent()
+            self.popupVC?.view.removeFromSuperview()
+        }
+    }
+}
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    var tabView1: tab1!
+    var tabView2: tab2!
+    var customTabBarContainer: UIView!
+    var isTab2Visible = false
+    var customDelegate: delegateFunc?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabViews()
+        showTab1()
+    }
+
+    func setupTabViews() {
+        let height: CGFloat = 80
+        customTabBarContainer = UIView()
+        customTabBarContainer.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(customTabBarContainer)
+
+        NSLayoutConstraint.activate([
+            customTabBarContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            customTabBarContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            customTabBarContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            customTabBarContainer.heightAnchor.constraint(equalToConstant: height)
+        ])
+
+        if let view1 = Bundle.main.loadNibNamed("tab1", owner: self, options: nil)?.first as? tab1 {
+            tabView1 = view1
+            tabView1.tap = customDelegate
+            tabView1.frame = customTabBarContainer.bounds
+        }
+
+        if let view2 = Bundle.main.loadNibNamed("tab2", owner: self, options: nil)?.first as? tab2 {
+            tabView2 = view2
+            tabView2.frame = customTabBarContainer.bounds
+        }
+    }
+
+    func toggleTabs() {
+        isTab2Visible.toggle()
+        isTab2Visible ? showTab2() : showTab1()
+    }
+
+    func showTab1() {
+        customTabBarContainer.subviews.forEach { $0.removeFromSuperview() }
+        customTabBarContainer.addSubview(tabView1)
+    }
+
+    func showTab2() {
+        customTabBarContainer.subviews.forEach { $0.removeFromSuperview() }
+        customTabBarContainer.addSubview(tabView2)
+    }
+}
+
